61825eec38617640026e9ca8064b12858cce4720
Jayanta66 <jayhalder18@gmail.com>
2025-06-20 13:26:57 +0530

    add

---
+++ src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -0,0 +1,39 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+	
+	/*
+	 * 
+	 * 
+	 * 
+	
+	{
+      "categoryName": "mobile"
+
+
+    }
+    
+    
+    ###############################################
+	
+	{
+	      "productId": 652,
+	      "productName": "Iphone Xs max",
+	      "image": "https://placehold.co/600x400",
+	      "description": "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
+	      "quantity": 0,
+	      "price": 1450.0,
+	      "discount": 10.0,
+	      "specialPrice": 1305.0
+	    }
+	*/
+
+}
